dd46175a01a451ead7403c652a7202a48036a3e1
“Anais <ananavelez@gmail.com>
2017-06-06 22:47:44 -0400

    haciendo style

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,8 +1,8 @@
 function mifunction(){
-    var x = document.getElementById("topnav");
-    if (x.className === "contenedor") {
-        x.className += " responsive";
-    } else {
-        x.className = "contenedor";
-    }
+	var x = document.getElementById("topnav");
+	if (x.className === "contenedor") {
+		x.className += " responsive";
+	} else {
+		x.className = "contenedor";
+	}
 }
